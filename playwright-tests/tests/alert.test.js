cb9894a4c972870df98d9a308cc05df71472647c
sonamgupta21 <sonam2001gupta@gmail.com>
2024-09-25 13:07:16 +0530

    both the alerts

--- playwright-tests/tests/alert.test.js
+++ playwright-tests/tests/alert.test.js
@@ -2,10 +2,10 @@ const { test, expect } = require('@playwright/test');
 const { createAlert } = require('./common-functions');
 
 test.describe('Alert Tests', () => {
-    // test('Create a new logs alert', async ({ page }) => {
-    //     await createAlert(page, 'Logs', 'option-1', 'option-1', 'city=Boston | stats count AS Count BY weekday');
-    //     await page.close();
-    // });
+    test('Create a new logs alert', async ({ page }) => {
+        await createAlert(page, 'Logs', 'option-1', 'option-1', 'city=Boston | stats count AS Count BY weekday');
+        await page.close();
+    });
 
     test('Create a new metrics alert', async ({ page }) => {
         await createAlert(page, 'Metrics', 'option-2');
